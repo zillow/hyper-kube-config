96050acbac6707d37192a1c8459d7d314744dbcf
zane <zane.williamson@gmail.com>
2018-09-11 20:45:46 -0700

    pylint fixes

--- add_cluster.py
+++ add_cluster.py
@@ -1,28 +1,28 @@
-import boto3
 import json
 import os
+import boto3
 
 dynamodb = boto3.resource('dynamodb')
 cluster_table_name = os.environ['DYNAMODB_TABLE_K8_CLUSTERS']
 cluster_table = dynamodb.Table(cluster_table_name)
 secrets_client = boto3.client('secretsmanager')
 
-
 def add_cluster(event, context):
+    """Add cluster and initial credentials"""
+
     validate_config_input(event['body'])
     cluster_config = json.loads(event['body'])
 
     try:
         cluster_name = cluster_config['clusters'][0]['name']
         cluster_server = cluster_config['clusters'][0]['cluster']['server']
-        
-    except KeyError as e:
-        print(f'Invalid cluster config: {e}')
+    except KeyError as err:
+        print(f'Invalid cluster config: {err}')
 
-        raise e
+        raise err
     # Put into dynamodb cluster info
 
-    if validate_unique_cluster_name(cluster_table_name, cluster_name) == None:
+    if validate_unique_cluster_name(cluster_table_name, cluster_name) is None:
         cluster_table.put_item(
             Item={
                 'id': cluster_name,
@@ -30,8 +30,8 @@ def add_cluster(event, context):
             }
         )
         # Put into secrets manager
-        user_name = cluster_config['users'][0]['user']['username'] 
-        password  = cluster_config['users'][0]['user']['password'] 
+        user_name = cluster_config['users'][0]['user']['username']
+        password  = cluster_config['users'][0]['user']['password']
         user_client_key = cluster_config['users'][0]['user']['client-key-data']
         user_client_certificate_data = cluster_config['users'][0]['user']['client-certificate-data']
 
@@ -39,12 +39,12 @@ def add_cluster(event, context):
             Name=f'user-{user_name}-{cluster_name}',
             SecretString=password
         )
-        
+
         secrets_client.create_secret(
             Name=f'user-client-key-{user_name}-{cluster_name}',
             SecretString=user_client_key
         )
-        
+
         secrets_client.create_secret(
             Name=f'user-client-certificate-data-{user_name}-{cluster_name}',
             SecretString=user_client_certificate_data
@@ -56,22 +56,23 @@ def add_cluster(event, context):
                 {"message": f'Cluster and config added {cluster_name}'}
             ),
         }
-    else:
-        return {
-            "statusCode": 404,
-            "body": json.dumps(
-                {"message": f'Cluster {cluster_name} already exists'}
-            )
-        }
+    return {
+        "statusCode": 404,
+        "body": json.dumps(
+            {"message": f'Cluster {cluster_name} already exists'}
+        )
+    }
 
 
 def validate_config_input(config):
+    """Validate config input is JSON"""
     try:
         json.loads(config)
     except ValueError as err:
         print(f'K8s config is not valid json error: {err}')
 
 def validate_unique_cluster_name(cluster_table_name, cluster_name):
+    """Validate cluster is a unique name"""
     try:
         item = cluster_table.get_item(Key={"id": cluster_name})
         print(f"Cluster {cluster_name} already exists: {item['Item']}")
