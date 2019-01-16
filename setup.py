2eabe304fa87c2d6db8249de435e24ba1f7f0163
zane <zane@ugh.cloud>
2019-01-15 21:05:20 -0800

    upping version to 0.0.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ URL = 'https://github.com/silvermullet/hyper-kube-config'
 EMAIL = 'zane@ugh.cloud'
 AUTHOR = 'Zane Williamson'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
